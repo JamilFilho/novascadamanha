4863c4e08fe349963e35911ad29a2a10d4ff2938
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-27 20:58:26 -0300

    feat: adicionando o next-sitemap ao projeto

---
+++ next-sitemap.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteUrl: 'https://novasdecadamanha.com.br/',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://novasdecadamanha.com.br/sitemap.xml',
+    ],
+  },
+}
