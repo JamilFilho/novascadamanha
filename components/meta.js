9e620657b0c6e5056840bf2b42486379a36120c4
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-28 18:32:40 -0300

    fix: corrigindo o caminho do manifest.json

--- components/meta.js
+++ components/meta.js
@@ -16,7 +16,7 @@ export default function Meta({post}) {
         <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-        <link rel='manifest' href='/static/manifest.json' />
+        <link rel='manifest' href='/manifest.json' />
         <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#282c35' />
         <link rel='shortcut icon' href='/icons/favicon.ico' />
 
