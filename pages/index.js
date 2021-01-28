790394cae6a989da48ec2a0fb5d37c8c7b37b075
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-28 18:29:02 -0300

    chore: atualizando as meta tags da home page

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,26 @@ export default function Index({ allPosts }) {
       <Head>
         <title>Novas de Cada Manhã | Devocionais diários para iniciar sua manhã.</title>
 
-        <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+        <meta name='application-name' content='Novas de Cada Manhã' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content='Novas de Cada Manhã' />
+        <meta name='description' content='Devocionais diários para iniciar sua manhã.' />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-config' content='/browserconfig.xml' />
+        <meta name='msapplication-TileColor' content='#282c35' />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta name='theme-color' content='#282c35' />
+
+        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='manifest' href='/static/manifest.json' />
+        <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#282c35' />
+        <link rel='shortcut icon' href='/icons/favicon.ico' />
         
+        <script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
         <Ads/>
       </Head>
 
