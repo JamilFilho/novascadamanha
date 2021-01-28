4b739e9351af258ea934cfe122e79a9ca0c879a8
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-28 19:08:39 -0300

    fix: corrigindo a importação do sharer.js

--- pages/[slug].js
+++ pages/[slug].js
@@ -24,23 +24,9 @@ export default function Post({ post }) {
 
       <meta name="title" content={`${post.title} | Novas de Cada Manhã `} />
       <meta name="description" content={post.excerpt} />
-
-      {/* <meta property="og:type" content="article" />
-      <meta property="article:author" content={`${post.author.name}`} />
-      <meta property="article:published_time" content={post.date}></meta>
-
-      <meta property="og:url" content={`https://novasdecadamanha.com.br/${post.slug}`} />
-      <meta property="og:title" content={`${post.title} | Novas de Cada Manhã `} />
-      <meta property="og:description" content={post.excerpt} />
-      <meta property="og:image" content={post.ogImage.url} />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={`https://novasdecadamanha.com.br/${post.slug}`} />
-      <meta property="twitter:title" content={`${post.title} | Novas de Cada Manhã `} />
-      <meta property="twitter:description" content={post.excerpt} />
-      <meta property="twitter:image" content={post.ogImage.url} /> */}
       <Meta post={post}/>
       <Ads />
+      <script src="/assets/scripts/sharer.min.js"></script>
     </Head>
     <Header />
     {router.isFallback ? (
