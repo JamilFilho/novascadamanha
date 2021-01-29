29f34d0b8b2c2ad9e1c92a43e8ad123fba22de29
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-29 14:28:20 -0300

    feat: adicionando navegação
    implementando navegação desktop
    
    re #5

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <nav className="fixed w-full h-20 flex flex-row items-center justify-center bg-white shadow z-50">
+    <nav className="fixed w-full h-20 flex flex-row items-center justify-between px-4 bg-white shadow z-50">
       <Link href="/">
         <a>
           <Image 
@@ -14,6 +14,25 @@ export default function Header() {
           />
         </a>
       </Link>
+      <ul className="hidden md:flex md:w-8/12">
+        <li className="mx-4">
+          <Link href="/devocionais">
+            <a className="font-medium">Devocionais</a>
+          </Link>
+        </li>
+        
+        <li className="mx-4">
+          <Link href="/newsletter/devocional-whatsapp">
+            <a className="font-medium">Devocional no WhatsApp</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/editorial/sobre">
+            <a className="font-medium">Sobre</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
