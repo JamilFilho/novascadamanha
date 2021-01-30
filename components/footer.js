04a83427c9814cf3b2ef2dca0ae3561c939731de
Jamil Filho <jamilfilho@outlook.com.br>
2021-01-29 21:12:51 -0300

    chore: adicionando links de navegação no rodapé e ajustando a navegação na header
    re #5

--- components/footer.js
+++ components/footer.js
@@ -3,17 +3,41 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <footer className="p-12 flex items-center justify-center bg-brand-background">
-      <Link href="/">
-        <a>
-          <Image 
-            src="/assets/images/site-logo.png"
-            alt="Novas de Cada Manhã"
-            width={250}
-            height={37.9}
-          />
-        </a>
-      </Link>
+    <footer className="p-12 bg-brand-background">
+      <div className="w-full flex flex-col-reverse md:flex-row items-center justify-center">
+        <div className="w-full md:w-1/4 flex items-center justify-center">
+          <Link href="/">
+            <a>
+              <Image 
+                src="/assets/images/site-logo.png"
+                alt="Novas de Cada Manhã"
+                width={250}
+                height={37.9}
+              />
+            </a>
+          </Link>
+        </div>
+
+        <ul className="w-full md:w-3/4 text-center flex flex-col mb-10 md:mb-0 md:flex-row justify-end">
+          <li className="mb-8 md:mb-0">
+            <Link href="/">
+              <a className="text-white md:mr-6">Fale Conosco</a>
+            </Link>
+          </li>
+
+          <li className="mb-8 md:mb-0">
+            <Link href="/">
+              <a className="text-white md:mr-6">Termos de Uso</a>
+            </Link>
+          </li>
+
+          <li className="mb-8 md:mb-0">
+            <Link href="/">
+              <a className="text-white">Política de Privacidade</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   )
 }
--- components/header.js
+++ components/header.js
@@ -3,18 +3,20 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <nav className="fixed w-full h-20 flex flex-row items-center justify-between px-4 bg-white shadow z-50">
-      <Link href="/">
-        <a>
-          <Image 
-            src="/assets/images/site-logo.png"
-            alt="Novas de Cada Manhã"
-            width={250}
-            height={37.9}
-          />
-        </a>
-      </Link>
-      <ul className="hidden md:flex md:w-8/12">
+    <nav className="fixed w-full h-20 flex flex-row items-center justify-between px-12 bg-white shadow z-50">
+      <div className="md:w-1/4">
+        <Link href="/">
+          <a>
+            <Image 
+              src="/assets/images/site-logo.png"
+              alt="Novas de Cada Manhã"
+              width={250}
+              height={37.9}
+            />
+          </a>
+        </Link>
+      </div>
+      <ul className="hidden md:flex md:w-3/4">
         <li className="mx-4">
           <Link href="/devocionais">
             <a className="font-medium">Devocionais</a>
