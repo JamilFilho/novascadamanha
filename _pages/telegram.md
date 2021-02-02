08cda62cf0b9c014c80f788bf059972a52faca9d
Jamil Filho <jamilfilho@outlook.com.br>
2021-02-02 16:21:10 -0300

    chore: updating devotional whatsapp page and create devotional telegram page

---
+++ _pages/telegram.md
@@ -0,0 +1,19 @@
+---
+title: 'Devocional no Telegram'
+excerpt: 'Deseja receber nossos devocionais diários diretamente em seu Telegram? Cadastre-se gratuitamente'
+buttonText: 'Entrar na lista'
+buttonLink: 'https://t.me/novasdecadamanha_devocional'
+headerColor: 'blue-500'
+ogImage:
+  url: '/assets/thumbnails/devocional-whatsapp.png'
+---
+
+### Perguntas frequentes
+
+**1. O  Novas de Cada Manha no Telegram é um grupo?**
+
+Não, o Novas de Cada Manhã no Telegram é apenas um canal de transmissão.
+
+**2. Posso compartilhar os devocionais com meus contatos do WhatsApp?**
+
+Sim, claro que pode! E é exatamente por esse motivo enviamos nossos devocionais pelo Telegram: para que você possa compartilhar com todos os seus amigos.
--- pages/devocional/[slug].js
+++ pages/devocional/[slug].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
+import Footer from '../../components/footer'
 import Header from '../../components/header'
 import { getPageBySlug, getAllPages } from '../../lib/apiPage'
 import Head from 'next/head'
@@ -21,8 +22,6 @@ export default function Post({ post }) {
       <meta name="description" content={post.excerpt} />
 
       <meta property="og:type" content="article" />
-      <meta property="article:author" content={`${post.author.name}`} />
-      <meta property="article:published_time" content={post.date}></meta>
 
       <meta property="og:url" content={`https://novasdecadamanha.com.br/${post.slug}`} />
       <meta property="og:title" content={`${post.title} | Novas de Cada Manhã `} />
@@ -36,24 +35,23 @@ export default function Post({ post }) {
       <meta property="twitter:image" content={post.ogImage.url} />
       <Ads />
     </Head>
-    <Header />
-    {router.isFallback ? (
-        <h1>Carregando...</h1>
-      ) : (
-    <>
-      <article className="flex flex-col lg:flex-row">
-        <div className="h-screen lg:h-screen lg:fixed lg:left-0 lg:top-0 lg:w-1/2 pt-20 flex flex-col items-center justify-center bg-green-500">
-          <p className="px-4 text-xl text-center font-medium text-white mb-8">Deseja receber nossos devocionais diários diretamente em seu WhatsApp? Cadastre-se gratuitamente</p>
-          <a class="text-xl px-6 py-4 rounded text-green-500 bg-white" href="https://api.whatsapp.com/send?phone=5516999861874&text=Desejo%20receber%20os%20devocionais%20pelo%20WhatsApp">
-            Cadastrar WhatsApp
-          </a>
-        </div>
 
-        <div className="lg:absolute lg:right-0 lg:top-0 lg:mt-20 py-12 px-10 lg:w-1/2">
-          <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: post.content }} />
-        </div>
-      </article>
-    </>
+    <Header />
+    {router.isFallback ? (<h1>Carregando...</h1>) : (
+      <>
+        <article className="flex flex-col lg:flex-row">
+          <div className={`h-screen lg:h-screen lg:fixed lg:left-0 lg:top-0 lg:w-1/2 pt-20 flex flex-col items-center justify-center bg-${post.headerColor}`}>
+            <p className="px-4 text-xl text-center font-medium text-white mb-8">{post.excerpt}</p>
+            <a class={`text-xl px-6 py-4 rounded text-${post.headerColor} bg-white`} href={post.buttonLink} target="_blanck">
+              {post.buttonText}
+            </a>
+          </div>
+
+          <div className="lg:absolute lg:right-0 lg:top-0 lg:mt-20 py-12 px-10 lg:w-1/2">
+            <div className={markdownStyles['markdown']} dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+        </article>
+      </>
     )}
     </>
   )
@@ -63,9 +61,10 @@ export async function getStaticProps({ params }) {
   const post = getPageBySlug(params.slug, [
     'title',
     'slug',
-    'date',
+    'buttonText',
+    'buttonLink',
+    'headerColor',
     'excerpt',
-    'author',
     'ogImage',
     'content'
   ])
