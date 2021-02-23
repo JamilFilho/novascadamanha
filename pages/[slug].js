31771e8c8e6c1892c3197e4b52242699b1e71696
Jamil Filho <jamilf.alien@gmail.com>
2021-02-23 17:32:59 -0300

    feat: adding Amazon books - static

--- pages/[slug].js
+++ pages/[slug].js
@@ -10,7 +10,6 @@ import { getPostBySlug, getAllPosts } from '../lib/api'
 import Head from 'next/head'
 import markdownToHtml from '../lib/markdownToHtml'
 import Meta from '../components/meta'
-import SubscribeForm from '../components/form'
 import AmazonBanner from '../components/amazon'
 
 export default function Post({ post }) {
@@ -39,8 +38,12 @@ export default function Post({ post }) {
         <Layout>
           <PostBody content={post.content} author={post.author} />
           
-          <div className=" w-10/12 lg:max-w-3xl mx-auto">
-            <SubscribeForm bgColor="bg-gray-100" padding="py-20 px-6" textSize="text-2xl"/>
+          <div className="w-11/12 lg:hidden mx-auto flex flex-row flex-wrap justify-around">
+            <iframe style={{width:'120px', height:'240px' }} marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=BR&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=novascadamanha-20&language=pt_BR&marketplace=amazon&region=BR&placement=6586027403&asins=6586027403&linkId=c8d2b019a43ee11c630c5872462710d9&show_border=true&link_opens_in_new_window=true"></iframe>
+
+            <iframe style={{width:'120px', height:'240px' }}  marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=BR&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=novascadamanha-20&language=pt_BR&marketplace=amazon&region=BR&placement=8543304652&asins=8543304652&linkId=71619acd0401f5d512b636901373fa16&show_border=true&link_opens_in_new_window=true"></iframe>
+
+            <iframe style={{width:'120px', height:'240px' }}  marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=BR&source=ss&ref=as_ss_li_til&ad_type=product_link&tracking_id=novascadamanha-20&language=pt_BR&marketplace=amazon&region=BR&placement=8543302358&asins=8543302358&linkId=722c439ec18f4433279704c4b620550e&show_border=true&link_opens_in_new_window=true"></iframe>
           </div>
 
           <div className="w-10/12 lg:max-w-3xl mx-auto mt-12">
